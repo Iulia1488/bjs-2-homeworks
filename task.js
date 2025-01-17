7ff880b2e97c46d6516509e3c945b28f984934e6
Iulia <ul4ik17@gmail.com>
2025-01-17 22:51:33 +0300

    commit1

--- task.js
+++ task.js
@@ -1,14 +1,29 @@
-function compareArrays(arr1, arr2) {
-    return ((arr1.length === arr2.length) && arr1.every((element, index) => element === arr2[index]));
+function Student(name, gender, age) {
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
-function getUsersNamesInAgeRange(users, gender) {
-  let result = users.filter(user => user.gender === gender).map(old => old.age).reduce((acc, old, index, arr) => {
-    acc += old;
-    if (index === arr.length - 1) {
-        return acc / arr.length;
-    }
-    return acc;
-  }, 0);
-  return result;
+Student.prototype.setSubject = function (subjectName) {
+  this.subject = subjectName;
+}
+
+Student.prototype.addMark = function (mark) {
+  (this.marks === undefined) ? this.marks = [mark] : this.marks.push(mark);
+}
+
+Student.prototype.addMarks = function (...marks) {
+  (this.marks === undefined) ? this.marks = [...marks] : this.marks.push(...marks);
+}
+
+Student.prototype.getAverage = function () {
+  let sum = 0;
+  this.marks.forEach(item => sum += item);
+  return sum / this.marks.length;
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
 }
